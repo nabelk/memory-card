41fc786996cefd835f962ad44f20de8734cbe317
Nabil Khalid <holanabilkhalid@gmail.com>
2024-02-01 04:34:22 +0800

    chore: set up initial component imports in App.jsx and import App in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,18 @@
 import { useState } from 'react';
 import './App.css';
+import { ScoreBoard } from './components/scoreboard';
+import { CardBoard } from './components/card-board';
+
+export function App() {
+    const [scores, setScores] = useState([
+        { name: 'curtScore', score: 0 },
+        { name: 'bestScore', score: 0 },
+    ]);
+
+    return (
+        <>
+            <ScoreBoard scores={scores}></ScoreBoard>
+            <CardBoard></CardBoard>
+        </>
+    );
+}
