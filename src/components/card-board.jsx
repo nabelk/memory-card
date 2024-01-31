5631c44d483a70604a1726a5c96f9ee9e9afd45e
Nabil Khalid <holanabilkhalid@gmail.com>
2024-02-01 04:43:22 +0800

    feat: implement async function for fetching data from multiple APIs at all once

--- src/components/card-board.jsx
+++ src/components/card-board.jsx
@@ -7,10 +7,41 @@ export function CardBoard() {
         async function fetchData(id) {
             const response = await fetch(
                 `https://project-heist-rahulv07.vercel.app/characters/${id}`,
-                { mode: 'cors' },
+                {
+                    mode: 'cors',
+                },
             );
-            const characterData = response.json();
+            const characterData = await response.json();
             return characterData;
         }
+
+        async function fetchAll(maxId) {
+            const promises = [];
+            for (let i = 1; i <= maxId; i++) {
+                promises.push(fetchData(i));
+            }
+            const data = await Promise.all(promises);
+            if (data.length !== maxId) return fetchAll(maxId);
+            return data;
+        }
+
+        fetchAll(21).then((data) => setCardData(data));
     }, []);
+
+    const listCards = cardData.map((card) => (
+        <div key={card.id}>
+            <img
+                style={{ width: '200px', height: '200px' }}
+                src={`/img/${card.id}.jpg`}
+                alt={`${card.alias || card.name}-image`}
+            ></img>
+            <p>{card.alias || card.name}</p>
+        </div>
+    ));
+
+    return (
+        <>
+            <div>{listCards}</div>
+        </>
+    );
 }
